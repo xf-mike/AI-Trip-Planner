5140d65105b981392e3bf6af43d4c41dabe041e9
xf-mike <x9fang@ucsd.edu>
2025-10-08 15:29:36 -0700

    fix deploy

--- run_ec2.sh
+++ run_ec2.sh
@@ -1,13 +1,13 @@
-sudo apt update &
-sudo apt install -y npm &
-cd frontend/ &
-npm i &
-npm run build &
-mv dist ../backend/ &
-sudo apt install -y python3-pip python3-venv &
-python3 -m venv .venv &
-source .venv/bin/activate &
-pip install --upgrade pip &
-pip install -r requirements.txt &
+sudo apt update
+sudo apt install -y npm
+cd frontend/
+npm i
+npm run build
+mv dist ../backend/
+sudo apt install -y python3-pip python3-venv
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
 # export OPENAI_API_KEY=(Your own key)
 python production.py
